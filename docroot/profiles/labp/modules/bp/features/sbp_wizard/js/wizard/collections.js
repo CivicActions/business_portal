e28ab1e2739714fb87c6ecc508537650922522ea
Alex Scott <alex@alexscott.net>
2016-04-11 18:10:01 +0200

    Adding update wizard JS files

---
+++ docroot/profiles/labp/modules/bp/features/sbp_wizard/js/wizard/collections.js
@@ -0,0 +1,29 @@
+/////////////////
+// Collections //
+/////////////////
+
+var namespace = namespace ||  {};
+namespace.collections = {}
+
+namespace.collections.Screens = Backbone.Collection.extend({
+
+  model: namespace.models.Screen,
+
+  url: "/api/json/business-portal-wizard",
+
+  chosen: function () {
+    return this.where({chosen: true});
+  },
+
+});
+
+
+//////////////
+// Sections //
+//////////////
+
+namespace.collections.Sections = Backbone.Collection.extend({
+    model: namespace.models.Section
+});
+
+namespace.collections.sections = new namespace.collections.Sections();
