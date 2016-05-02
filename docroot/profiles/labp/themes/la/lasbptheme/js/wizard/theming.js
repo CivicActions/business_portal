2751e73a077e8caaf404de6a913ac5e33d692c58
Adam Bergstein <nerdstein@gmail.com>
2016-05-01 21:59:29 -0400

    Hostname checking

--- docroot/profiles/labp/themes/la/lasbptheme/js/wizard/theming.js
+++ docroot/profiles/labp/themes/la/lasbptheme/js/wizard/theming.js
@@ -1,7 +1,14 @@
 (function($) {
   $( document ).ready(function(){
     $('body').addClass('wizard-page');
-    if (window.location.hostname == 'lacity.org' || window.location.hostname == 'acsitefactory.com') {
+    var hostnames = ["labusinessportal.dev2-cityofla.acsitefactory.com", "business.lacity.org", "labp.cityofla.acsitefactory.com"];
+    var ishosted = false;
+    for (var ind = 0; ind < hostnames.length; ind++) {
+      if (hostnames[ind] == window.location.hostname) {
+        ishosted = true;
+      }
+    }
+    if (ishosted) {
       var logofilename = "/profiles/labp/themes/la/labusinessportaltheme/logo-dark.png";
     } else {
       var logofilename = "/profiles/labp/themes/la/lasbptheme/logo-dark.png";
