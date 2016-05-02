2f22d980c9738d33dd4cb1cc06d39b700a6550d0
Adam Bergstein <nerdstein@gmail.com>
2016-05-01 21:45:35 -0400

    Adding theme checking by domain for logo wizard swap

--- docroot/profiles/labp/themes/la/lasbptheme/js/wizard/theming.js
+++ docroot/profiles/labp/themes/la/lasbptheme/js/wizard/theming.js
@@ -1,7 +1,11 @@
 (function($) {
   $( document ).ready(function(){
     $('body').addClass('wizard-page');
-    var logofilename = "/profiles/labp/themes/la/lasbptheme/logo-dark.png";
+    if (window.location.hostname != 'lacity.org' || window.location.hostname != 'acsitefactory.com') {
+      var logofilename = "/profiles/labp/themes/la/labusinessportaltheme/logo-dark.png";
+    } else {
+      var logofilename = "/profiles/labp/themes/la/lasbptheme/logo-dark.png";
+    }
     $('.header__logo img').attr("src",logofilename);
   });
 })(jQuery);
