a86fca71d39bf29cce590f3ccc21541932c779f0
Adam Bergstein <nerdstein@gmail.com>
2016-05-01 21:47:13 -0400

    Updating wizard logo path logic

--- docroot/profiles/labp/themes/la/lasbptheme/js/wizard/theming.js
+++ docroot/profiles/labp/themes/la/lasbptheme/js/wizard/theming.js
@@ -1,7 +1,7 @@
 (function($) {
   $( document ).ready(function(){
     $('body').addClass('wizard-page');
-    if (window.location.hostname != 'lacity.org' || window.location.hostname != 'acsitefactory.com') {
+    if (window.location.hostname == 'lacity.org' || window.location.hostname == 'acsitefactory.com') {
       var logofilename = "/profiles/labp/themes/la/labusinessportaltheme/logo-dark.png";
     } else {
       var logofilename = "/profiles/labp/themes/la/lasbptheme/logo-dark.png";
