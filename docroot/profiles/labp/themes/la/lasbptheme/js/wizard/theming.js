a94319baf359a326c2d87e541b17a59ca37fe5d1
Adam Bergstein <nerdstein@gmail.com>
2016-05-01 21:37:30 -0400

    Relative path fix for wizard logo

--- docroot/profiles/labp/themes/la/lasbptheme/js/wizard/theming.js
+++ docroot/profiles/labp/themes/la/lasbptheme/js/wizard/theming.js
@@ -1,7 +1,7 @@
 (function($) {
   $( document ).ready(function(){
     $('body').addClass('wizard-page');
-    var logofilename = "profiles/labp/themes/la/lasbptheme/logo-dark.png";
+    var logofilename = "/profiles/labp/themes/la/lasbptheme/logo-dark.png";
     $('.header__logo img').attr("src",logofilename);
   });
 })(jQuery);
