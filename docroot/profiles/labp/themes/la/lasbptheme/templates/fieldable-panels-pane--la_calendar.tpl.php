2a70587e3b861c3ec4ec3d1f818399933a89d06e
Adam Bergstein <nerdstein@gmail.com>
2016-06-28 09:53:58 -0400

    Cleanup - Calendar and search

--- docroot/profiles/labp/themes/la/lasbptheme/templates/fieldable-panels-pane--la_calendar.tpl.php
+++ docroot/profiles/labp/themes/la/lasbptheme/templates/fieldable-panels-pane--la_calendar.tpl.php
@@ -9,11 +9,19 @@
 ?>
 <?php
 
+//dpm($content);
+
 // Preprocessing field_unique_id.
 $unique_id = $content['field_unique_id'][0]['#markup'];
 
 // Preprocessing field_display_type.
 $display_type = $content['field_display_type']['#items'][0]['value'];
+
+$departments = '';
+if (!empty($content['field_departments']) and !empty($content['field_departments']['#items'])) {
+  $departments = $content['field_departments']['#items'][0]['value'];
+}
+
 
 if (user_is_logged_in()) {
 ?>
@@ -28,7 +36,6 @@ if (user_is_logged_in()) {
     src="http://calendar.dev2-cityofla.acsitefactory.com/app/loader.php?name=embed-calendar"
     data-version="v1"
     data-widget-id="0001"
-    data-display="<?php print $display_type; ?>"
     id="<?php print $unique_id; ?>"
     type="text/javascript" async>
     (function () {
@@ -36,7 +43,7 @@ if (user_is_logged_in()) {
         var s = document.createElement('script');
         s.type = 'text/javascript';
         s.async = true;
-        s.src = 'http://calendar.dev2-cityofla.acsitefactory.com/app/loader.php?name=embed-calendar';
+        s.src = 'http://calendar.lacity.org/app/loader.php?name=embed-calendar';
         var head = document.getElementsByTagName('head');
         (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(s);
       }
@@ -47,7 +54,17 @@ if (user_is_logged_in()) {
         window.addEventListener('load', la_cal_<?php print $unique_id; ?>_async_load, false);
     })();
   </script>
-  <div class="<?php print $unique_id; ?>-container" id="fullcalendar">Loading
+  <div class="<?php print $unique_id; ?>-container" id="fullcalendar"
+       data-display="<?php print $display_type; ?>"
+       data-showshare="0"
+       data-showmonth="1"
+    <?php
+     if (!empty($departments)) {
+       ?>
+       data-department="<?php print $departments; ?>"
+       <?php
+     }
+    ?>>Loading
   </div>
 </div>
 
