c71947dfb2b0962e844c080747c1e9fe2e7a6c10
Manoj Kumar <talk2manoj@gmail.com>
2016-08-02 15:28:01 +0530

    minor javascript issue fixes

--- docroot/profiles/labp/themes/la/labusinessportaltheme/js/script.js
+++ docroot/profiles/labp/themes/la/labusinessportaltheme/js/script.js
@@ -295,24 +295,24 @@ Drupal.behaviors.rotator = {
     return this.height( Math.max.apply(this, $(this).map(function(i,e){return $(e).height();}).get() ) );
   };
   
-  $(document).ready(function() {
-    // Equalize columns on page load
-    var $panels = $('.center-wrapper .panel-panel');
-    $(window).load(function() {
-      if ($(window).width() > 959) {
-        $panels.equalizeHeights();
-      }
-    });
-    
-    // Equalize columns on page resize
-    $(window).resize(function() {
-      if ($(window).width() > 959) {
-        $panels.equalizeHeights();
-      } else {
-        $panels.css({'height': 'auto'});
-      }
-    });
-  });
+//  $(document).ready(function() {
+//    // Equalize columns on page load
+//    var $panels = $('.center-wrapper .panel-panel');
+//    $(window).load(function() {
+//      if ($(window).width() > 959) {
+//        $panels.equalizeHeights();
+//      }
+//    });
+//    
+//    // Equalize columns on page resize
+//    $(window).resize(function() {
+//      if ($(window).width() > 959) {
+//        $panels.equalizeHeights();
+//      } else {
+//        $panels.css({'height': 'auto'});
+//      }
+//    });
+//  });
   
   /**
    * Code to make search and translation icon clickable to show/hide search form
